55191c17981405c658e35f127594652601b4d65e
Josue Chavarria M <josuechavarria98@gmail.com>
2022-06-16 21:29:08 -0600

    pageBuild

---
+++ docs/static/wRTX_BfWZ1ZJsbyHw-1m4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ae51ba48-374809820234541f.js","static/chunks/170-ff43600cea8931e1.js","static/chunks/pages/index-d325a3c63f87fa1c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
